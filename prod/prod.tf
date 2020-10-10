e99d3bd42ff050c6070136802df5a267690beb3b
Robert Kardjaliev <robk-dev@users.noreply.github.com>
2020-10-10 02:46:53 +0100

    decoupled static ip creation and assignment

--- prod/prod.tf
+++ prod/prod.tf
@@ -45,8 +45,12 @@ resource "aws_instance" "prod_web" {
   }  
 }
 
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.id
+  allocation_id = aws_eip.prod_web.id
+}
+
 resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
 
   tags = {
     "Terraform" = "true"
--- scripts/02_state.sh
+++ scripts/02_state.sh
@@ -3,4 +3,5 @@ terraform apply -auto-approve
 terraform state list
 terraform show -json
 terraform graph
+terraform graph | grep -v -e 'meta' -e 'close' -e 's3' -e 'vpc'
 # terraform destroy -auto-approve
